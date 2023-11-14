43bf5f3cbc91b17b7247c1bc9a0f7e496e84f229
Sarowar-hossen <sarowar1942@gmail.com>
2023-11-14 14:50:07 +0600

    deplay first time

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+require("dotenv").config()
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+app.listen(process.env.PORT,()=>{
+    console.log("Hello my friends")
+})
